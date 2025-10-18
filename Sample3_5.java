e66b552d294f7ba43e3af009c1cd4e3aaecf176d
justin19990416 <118785866+justin19990416@users.noreply.github.com>
2025-10-18 14:15:51 +0800

    347780 CODE CHAPTER3

---
+++ Sample3_5.java
@@ -0,0 +1,27 @@
+public class Sample3_5 {
+    public static void main(String[] args) {
+        Car car1;
+        car1 = new Car();
+
+        car1.num = 1234;
+        car1.gas = 20.5;
+
+        car1.showCar0();
+    }
+}
+
+// Car類別
+class Car {
+    int num;
+    double gas;
+
+    void show() {
+        System.out.println("車號是" + this.num);
+        System.out.println("汽油量是" + this.gas);
+    }
+
+    void showCar0() {
+        System.out.println("開始顯示車子資料");
+        this.show();
+    }
+}
